09dfab7bbb8fe9690f5ec86728d31aaceb020403
Kalpa Welivitigoda <kalpaw@wso2.com>
2014-07-08 17:08:46 +0530

    added CDI-Servlet sample

---
+++ CDI_Servlet/src/main/java/org/wso2/as/ee/HelloServlet.java
@@ -0,0 +1,19 @@
+package org.wso2.as.ee;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+
+    @Inject
+    private Greet greet;
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println(greet.greet());
+        writer.close();
+    }
+}
