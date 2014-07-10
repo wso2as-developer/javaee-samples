ba6801f903e7b020513c0b2256a89e73f1e175fd
Kalpa Welivitigoda <kalpaw@wso2.com>
2014-07-11 00:49:48 +0530

    @Decorator sample

---
+++ CDI-Servlet/sample6/src/main/java/org/wso2/as/ee/sample6/Greeter.java
@@ -0,0 +1,6 @@
+package org.wso2.as.ee.sample6;
+
+
+public interface Greeter {
+    public String greet();
+}
---
+++ CDI-Servlet/sample6/src/main/java/org/wso2/as/ee/sample6/Receptionist.java
@@ -0,0 +1,10 @@
+package org.wso2.as.ee.sample6;
+
+
+public class Receptionist implements Greeter {
+
+    @Override
+    public String greet() {
+        return "Hi, this is the Receptionist class";
+    }
+}
