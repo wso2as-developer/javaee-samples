ac8628a25ce90121da43d4206ba09686355ff59a
Kalpa Welivitigoda <callkalpa@gmail.com>
2014-07-21 16:15:45 +0530

    modified module info

--- CDI-Servlet/cdi-alternative/src/main/java/org/wso2/as/ee/sample2/HelloServlet.java
+++ CDI-Servlet/cdi-alternative/src/main/java/org/wso2/as/ee/sample2/HelloServlet.java
@@ -2,6 +2,7 @@
 
 package org.wso2.as.ee.sample2;
 
+import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.annotation.WebServlet;
@@ -14,8 +15,7 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
     @Inject
     private Greeter greeter;
 
-    @Inject
-    @Named("NamedImplementation")
+    @EJB
     private Greeter namedGreeter;
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
